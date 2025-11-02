87b95b6eff6979bc5d2e2112f788443b9880c172
Michael Kellner <mail@tiv73.com>
2025-11-02 10:51:53 +0100

    compatibility

--- ParserConsole/ConvertCommand.cs
+++ ParserConsole/ConvertCommand.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
